6a90004e58a4fdb59a1c353e4766df007c22d48d
pimp <wotik.com@gmail.com>
2023-03-17 11:14:18 +0300

    navbar profile image

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
     return (
         <div className={styles["root-container"]}>
             <Navbar />
+            {token}
             <main className={styles.main}>
                 <Outlet />
             </main>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import styles from "./Navbar.module.css";
 import logo from "../assets/avatar.svg";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
 import { FiMenu } from "react-icons/fi";
@@ -8,7 +8,24 @@ import useToken from "../hooks/useToken";
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
+    const [avatarURL, setAvatarURL] = useState<string>(logo);
     const [token] = useToken();
+
+    useEffect(() => {
+        if (!token) return;
+
+        fetch("https://teamder-dev.herokuapp.com/api/mock/img", {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                Accept: "image/jpeg",
+            },
+        })
+            .then((res) => res.blob())
+            .then((image) => setAvatarURL(URL.createObjectURL(image)))
+            .catch((err) => console.error(err));
+
+        return () => URL.revokeObjectURL(avatarURL);
+    }, [token]);
 
     const clickHandler = () => setIsOpen((prevIsOpen) => !prevIsOpen);
     const isMobile = useMediaQuery({ maxWidth: 768 });
@@ -62,33 +79,43 @@ export default function Navbar() {
                             Dashboard
                         </NavLink>
                     </div>
-                    {!token && (
-                        <div className={styles["right-side"]}>
-                            <NavLink
-                                to="/signin"
-                                className={({ isActive }) =>
-                                    `${styles["nav-link"]}  ${
-                                        isActive ? styles.active : ""
-                                    }`
-                                }
-                            >
-                                Sign In
-                            </NavLink>
-                            <NavLink
-                                to={{
-                                    pathname: "/signin",
-                                    search: "?signup=true",
-                                }}
-                                className={({ isActive }) =>
-                                    `${styles["nav-link"]} ${
-                                        styles["sign-up"]
-                                    } ${isActive ? styles.active : ""}`
-                                }
-                            >
-                                Sign Up
-                            </NavLink>
-                        </div>
-                    )}
+                    <div className={styles["right-side"]}>
+                        {!token && (
+                            <>
+                                <NavLink
+                                    to="/signin"
+                                    className={({ isActive }) =>
+                                        `${styles["nav-link"]}  ${
+                                            isActive ? styles.active : ""
+                                        }`
+                                    }
+                                >
+                                    Sign In
+                                </NavLink>
+                                <NavLink
+                                    to={{
+                                        pathname: "/signin",
+                                        search: "?signup=true",
+                                    }}
+                                    className={({ isActive }) =>
+                                        `${styles["nav-link"]} ${
+                                            styles["sign-up"]
+                                        } ${isActive ? styles.active : ""}`
+                                    }
+                                >
+                                    Sign Up
+                                </NavLink>
+                            </>
+                        )}
+
+                        {token && (
+                            <img
+                                className={styles.avatar}
+                                src={avatarURL}
+                                alt="Avatar"
+                            />
+                        )}
+                    </div>
                 </div>
             )}
         </nav>
